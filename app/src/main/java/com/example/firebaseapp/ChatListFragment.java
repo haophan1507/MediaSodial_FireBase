b984f798a26f8af9ec39a7b3538c2888fa64bbb4
Phan Văn Hào <phanvanhao1507@gmail.com>
2021-03-11 15:09:57 +0700

    first commit

---
+++ app/src/main/java/com/example/firebaseapp/ChatListFragment.java
@@ -0,0 +1,185 @@
+package com.example.firebaseapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.view.MenuItemCompat;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.SearchView;
+
+import com.example.firebaseapp.adapters.AdapterChatlist;
+import com.example.firebaseapp.models.ModelChat;
+import com.example.firebaseapp.models.ModelChatList;
+import com.example.firebaseapp.models.ModelUsers;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChatListFragment extends Fragment {
+
+    FirebaseAuth firebaseAuth;
+    RecyclerView recyclerView;
+    List<ModelChatList> chatListList;
+    List<ModelUsers> usersList;
+    DatabaseReference reference;
+    FirebaseUser currentUser;
+    AdapterChatlist adapterChatlist;
+
+    public ChatListFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_chat_list, container, false);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        currentUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        recyclerView = view.findViewById(R.id.recyclerView);
+        chatListList = new ArrayList<>();
+        reference = FirebaseDatabase.getInstance().getReference("Chatlist").child(currentUser.getUid());
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                chatListList.clear();
+                for (DataSnapshot ds : snapshot.getChildren()){
+                    ModelChatList chatList = ds.getValue(ModelChatList.class);
+                    chatListList.add(chatList);
+                }
+                loadChats();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        return view;
+    }
+
+    private void loadChats() {
+        usersList = new ArrayList<>();
+        reference = FirebaseDatabase.getInstance().getReference("Users");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                usersList.clear();
+                for (DataSnapshot ds : snapshot.getChildren()){
+                    ModelUsers users = ds.getValue(ModelUsers.class);
+                    for (ModelChatList chatList : chatListList){
+                        if (users.getUid() != null && users.getUid().equals(chatList.getId())){
+                            usersList.add(users);
+                            break;
+                        }
+                    }
+                    adapterChatlist = new AdapterChatlist(getContext(), usersList);
+                    recyclerView.setAdapter(adapterChatlist);
+                    for (int i = 0; i<usersList.size(); i++){
+                        lastMessage(usersList.get(i).getUid());
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void lastMessage(String userId) {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Chats");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String theLastMessage = "default";
+                for (DataSnapshot ds : snapshot.getChildren()){
+                    ModelChat chat = ds.getValue(ModelChat.class);
+                    if (chat == null){
+                        continue;
+                    }
+                    String sender = chat.getSender();
+                    String receiver = chat.getReceiver();
+                    if (sender == null || receiver == null){
+                        continue;
+                    }
+                    if (chat.getReceiver().equals(currentUser.getUid()) && chat.getSender().equals(userId) ||
+                    chat.getReceiver().equals(userId) && chat.getSender().equals(currentUser.getUid())){
+                        if (chat.getType().equals("image")){
+                            theLastMessage = "Sent a photo";
+                        } else {
+                            theLastMessage = chat.getMessage();
+                        }
+                    }
+                }
+                adapterChatlist.setLastMessageMap(userId, theLastMessage);
+                adapterChatlist.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void checkUserStatus(){
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if (user != null){
+
+        }
+        else {
+            startActivity(new Intent(getActivity(), MainActivity.class));
+            getActivity().finish();
+        }
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_main, menu);
+
+        menu.findItem(R.id.action_add_post).setVisible(false);
+
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_logout) {
+            firebaseAuth.signOut();
+            checkUserStatus();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
